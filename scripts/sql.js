a2e1c5e4534d89ea14e87c3b6cb1a09a3bce0740
TryIng1Q <kpktdhovord@gmail.com>
2024-04-24 19:28:41 +0500

    change token

--- scripts/sql.js
+++ scripts/sql.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const conn = mysql.createConnection({
 	host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'PW£On5MNW32K',
   database: 'TelegramBotWithAdmins'
 });
 conn.connect((err) => {
